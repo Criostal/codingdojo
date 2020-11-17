5fa0be744ce8dffb3fc29e6dee1e52bae284e10c
Chris Curth <christian.curth@nexus-ag.de>
2020-11-17 09:07:16 +0100

    new unit tests

--- Hangman/Hangman/Hangman.cs
+++ Hangman/Hangman/Hangman.cs
@@ -10,29 +10,39 @@ namespace Hangman
     {
         private string _secret = string.Empty;
         private HashSet<char> _letters = new HashSet<char>();
+        int _attemps = 0;
 
         public Hangman(string secret)
         {
             _letters.Clear();
             _secret = secret;
+            _attemps = 10;
         }
 
         public string Guess(char letter)
         {
             var s = string.Empty;
             _letters.Add(letter);
+            
             foreach (var c in _secret)
             {
-                if( _letters.Contains(char.ToUpper(c)) )
+                if( _letters.Contains(char.ToUpper(c)) || _letters.Contains(char.ToLower(c)))
                 {
-                    s += char.ToUpper(c);
+                    s += c;
                 }
                 else
                 {
                     s += '-';
                 }
             }
+            _attemps--;
+
             return s;
+        }
+
+        public int RemainingAttemps()
+        {
+            return _attemps < 0 ? 0 : _attemps;
         }
     }
 }
--- Hangman/HangmanTest/HangmanTest.cs
+++ Hangman/HangmanTest/HangmanTest.cs
@@ -13,34 +13,34 @@ namespace Hangman
             var h = new Hangman("Developer");
             
             Assert.AreEqual("---------", h.Guess('A'));
-            Assert.AreEqual("-E-E---E-", h.Guess('E'));
-            Assert.AreEqual("-E-E---E-", h.Guess('I'));
-            Assert.AreEqual("-E-E-O-E-", h.Guess('O'));
-            Assert.AreEqual("-E-E-O-E-", h.Guess('U'));
-            Assert.AreEqual("-E-E-O-ER", h.Guess('R'));
-            Assert.AreEqual("-E-E-O-ER", h.Guess('N'));
-            Assert.AreEqual("-E-E-O-ER", h.Guess('S'));
-            Assert.AreEqual("-E-E-O-ER", h.Guess('T'));
-            Assert.AreEqual("-E-ELO-ER", h.Guess('L'));
-            Assert.AreEqual("-E-ELOPER", h.Guess('P'));
-            Assert.AreEqual("DE-ELOPER", h.Guess('D'));
-            Assert.AreEqual("DEVELOPER", h.Guess('V'));
+            Assert.AreEqual("-e-e---e-", h.Guess('E'));
+            Assert.AreEqual("-e-e---e-", h.Guess('I'));
+            Assert.AreEqual("-e-e-o-e-", h.Guess('O'));
+            Assert.AreEqual("-e-e-o-e-", h.Guess('U'));
+            Assert.AreEqual("-e-e-o-er", h.Guess('R'));
+            Assert.AreEqual("-e-e-o-er", h.Guess('N'));
+            Assert.AreEqual("-e-e-o-er", h.Guess('S'));
+            Assert.AreEqual("-e-e-o-er", h.Guess('T'));
+            Assert.AreEqual("-e-elo-er", h.Guess('L'));
+            Assert.AreEqual("-e-eloper", h.Guess('P'));
+            Assert.AreEqual("De-eloper", h.Guess('D'));
+            Assert.AreEqual("Developer", h.Guess('V'));
         }
 
         [DataTestMethod]
         [DataRow("---------", "A")]
-        [DataRow("-E-E---E-", "AE")]
-        [DataRow("-E-E---E-", "AEI")]
-        [DataRow("-E-E-O-E-", "AEIO")]
-        [DataRow("-E-E-O-E-", "AEIOU")]
-        [DataRow("-E-E-O-ER", "AEIOUR")]
-        [DataRow("-E-E-O-ER", "AEIOURN")]
-        [DataRow("-E-E-O-ER", "AEIOURNS")]
-        [DataRow("-E-E-O-ER", "AEIOURNST")]
-        [DataRow("-E-ELO-ER", "AEIOURNSTL")]
-        [DataRow("-E-ELOPER", "AEIOURNSTLP")]
-        [DataRow("DE-ELOPER", "AEIOURNSTLPD")]
-        [DataRow("DEVELOPER", "AEIOURNSTLPDV")]
+        [DataRow("-e-e---e-", "AE")]
+        [DataRow("-e-e---e-", "AEI")]
+        [DataRow("-e-e-o-e-", "AEIO")]
+        [DataRow("-e-e-o-e-", "AEIOU")]
+        [DataRow("-e-e-o-er", "AEIOUR")]
+        [DataRow("-e-e-o-er", "AEIOURN")]
+        [DataRow("-e-e-o-er", "AEIOURNS")]
+        [DataRow("-e-e-o-er", "AEIOURNST")]
+        [DataRow("-e-elo-er", "AEIOURNSTL")]
+        [DataRow("-e-eloper", "AEIOURNSTLP")]
+        [DataRow("De-eloper", "AEIOURNSTLPD")]
+        [DataRow("Developer", "AEIOURNSTLPDV")]
         public void TestMethodHangmanParameter(string expected, string input)
         {
             var h = new Hangman("Developer");
@@ -51,6 +51,73 @@ namespace Hangman
             }
 
             Assert.AreEqual(s, expected);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("secret", 'x', "------")]
+        [DataRow("apple", 'x', "-----")]
+        public void GuessWrongLetterShouldReturnDashedString(string secret, char guessedLetter, string solvedSecret)
+        {
+            Hangman h = new Hangman(secret);
+            Assert.AreEqual(solvedSecret, h.Guess(guessedLetter));
+        }
+
+        [DataTestMethod]
+        [DataRow("secret", 'e', "-e--e-")]
+        [DataRow("apple", 'e', "----e")]
+        [DataRow("apple", 'a', "a----")]
+        public void GuessCorrectLetterShouldReturnStringWithRevealedChar(string secret, char guessedLetter, string solvedSecret)
+        {
+            Hangman h = new Hangman(secret);
+            Assert.AreEqual(solvedSecret, h.Guess(guessedLetter));
+        }
+
+        [DataTestMethod]
+        [DataRow("secret", 'C', "--c---")]
+        [DataRow("apple", 'p', "-pp--")]
+        [DataRow("apple", 'L', "---l-")]
+        [DataRow("APPLE", 'l', "---L-")]
+        public void GuessedLetterIsFoundRegardlessTheCase(string secret, char guessedLetter, string solvedSecret)
+        {
+            Hangman h = new Hangman(secret);
+            Assert.AreEqual(solvedSecret, h.Guess(guessedLetter));
+        }
+   
+
+        [TestMethod]
+        public void OneGuessShouldReduceRemainingAttemptsByOne()
+        {
+            Hangman h = new Hangman("secret");
+
+            Assert.AreEqual("-e--e-", h.Guess('e'));
+            Assert.AreEqual(9, h.RemainingAttemps());
+            Assert.AreEqual("se--e-", h.Guess('s'));
+            Assert.AreEqual(8, h.RemainingAttemps());
+            Assert.AreEqual("se-re-", h.Guess('r'));
+            Assert.AreEqual(7, h.RemainingAttemps());
+            Assert.AreEqual("secre-", h.Guess('c'));
+            Assert.AreEqual(6, h.RemainingAttemps());
+            Assert.AreEqual("secret", h.Guess('t'));
+            Assert.AreEqual(5, h.RemainingAttemps());
+        }
+
+        [TestMethod]
+        public void NoMoreGuessingPossibleOnceNoAttemptIsLeft()
+        {
+            Hangman h = new Hangman("abc");
+
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(9, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(8, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(7, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(6, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(5, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(4, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(3, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(2, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(1, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x'));Assert.AreEqual(0, h.RemainingAttemps());
+            Assert.AreEqual("---", h.Guess('x')); Assert.AreEqual(0, h.RemainingAttemps());
         }
 
     }
